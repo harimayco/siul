51db4001829c68ebabf48f1696734048a9a3eda0
odhier <odhier1@gmail.com>
2020-03-01 13:21:27 +0700

    init

---
+++ app/Http/Controllers/ApiPostController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Session;
+use Request;
+use DB;
+use CRUDBooster;
+
+
+class ApiPostController extends \crocodicstudio\crudbooster\controllers\ApiController
+{
+
+    function __construct()
+    {
+        $this->table       = "blogs";
+        $this->permalink   = "post";
+        $this->method_type = "get";
+    }
+
+
+    public function hook_before(&$postdata)
+    {
+        //This method will be execute before run the main process
+
+    }
+
+    public function hook_query(&$query)
+    {
+        //This method is to customize the sql query
+        $request = request();
+        if (!empty($request->title)) {
+            $query->where('title', 'like', '%' . $request->title . '%');
+        }
+
+        return $query;
+    }
+
+    public function hook_after($postdata, &$result)
+    {
+        //This method will be execute after run the main process
+
+    }
+}
---
+++ database/migrations/2020_03_01_051318_add_blogs.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBlogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->longText('image')->nullable();
+            $table->longText('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
